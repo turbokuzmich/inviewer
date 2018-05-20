fdc46f5ea36c353c4e34f22db10f19df9359ecd0
Dmitry Kurteev <turbokuzmich@yandex-team.ru>
2018-05-21 01:01:21 +0300

    Object page WIP

--- front/src/js/index.js
+++ front/src/js/index.js
@@ -1,1 +1,30 @@
 import 'styles/index.scss';
+import Chart from 'chart.js';
+
+$(() => {
+    const $pieChart = $('.js-pie-chart');
+
+    $pieChart.each((_, elem) => {
+        const ctx = elem.getContext('2d');
+
+        var myPieChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: ['Africa', 'Asia', 'Europe', 'Latin America', 'North America'],
+                datasets: [
+                    {
+                        label: 'Population (millions)',
+                        backgroundColor: ['#3e95cd', '#8e5ea2', '#3cba9f', '#e8c3b9', '#c45850'],
+                        data: [2478, 5267, 734, 784, 433]
+                    }
+                ]
+            },
+            options: {
+                maintainAspectRatio: false,
+                legend: {
+                    display: false
+                }
+            }
+        });
+    });
+});
